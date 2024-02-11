a5d83c6a67cbe32b07e4e94ddf89e7857ae47644
abhinavan <abhinav.15oct1993@gmail.com>
2024-02-11 12:08:31 +0530

    commit

--- attachments/03 React Essentials/01-starting-project/src/components/Header.jsx
+++ attachments/03 React Essentials/01-starting-project/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import reactImg from "../assets/react-core-concepts.png";
+import "./css/Header.css";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
---
+++ attachments/03 React Essentials/01-starting-project/src/components/TabButtons.jsx
@@ -0,0 +1,8 @@
+const TabButtons = (props) => {
+  return (
+    <li>
+      <button>{props.children}</button>
+    </li>
+  );
+};
+export default TabButtons;
