180a634f302ea6d176c915b50c6259ba9937feb0
abhinavan <abhinav.15oct1993@gmail.com>
2024-02-29 08:24:45 +0530

    commited

---
+++ attachments/05 Essentials Practice/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import investmentLogo from "../assets/investment-calculator-logo.png";
+const Header = () => {
+  return (
+    <header id="header">
+      <img src={investmentLogo} alt="Investment"></img>
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
+export default Header;
---
+++ attachments/05 Essentials Practice/01-starting-project/src/components/UserInput.jsx
@@ -0,0 +1,15 @@
+const UserInput = () => {
+  return (
+    <div id="user-input" className="input-group">
+      <label>Name</label>
+      <input type="text" value="value"></input>
+      <label>Number</label>
+      <input type="number" value="value"></input>
+      <label>Third</label>
+      <input type="text" value="value"></input>
+      <label>Four</label>
+      <input type="four" value="value"></input>
+    </div>
+  );
+};
+export default UserInput;
